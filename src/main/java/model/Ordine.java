5e1fed89e19ec6f2c4d098598728c28f0a60d5c2
lucadomenico1 <lucad.chiappetta@gmail.com>
2026-05-12 19:33:30 +0200

    aggiunta Ordine

--- src/main/java/model/Ordine.java
+++ src/main/java/model/Ordine.java
@@ -8,10 +8,43 @@ public class Ordine {
 	private int idOrdine;
 	private Timestamp dataAcquisto;
 	private double totaleOrdine;
-	private String emailUtente; // La chiave esterna (Foreign Key) verso l'Utente
+	private String emailUtente; // La chiave esterna verso l'Utente
 	
 	// Costruttore vuoto
 	public Ordine() {
 	}
+
+	public int getIdOrdine() {
+		return idOrdine;
+	}
+
+	public void setIdOrdine(int idOrdine) {
+		this.idOrdine = idOrdine;
+	}
+
+	public Timestamp getDataAcquisto() {
+		return dataAcquisto;
+	}
+
+	public void setDataAcquisto(Timestamp dataAcquisto) {
+		this.dataAcquisto = dataAcquisto;
+	}
+
+	public double getTotaleOrdine() {
+		return totaleOrdine;
+	}
+
+	public void setTotaleOrdine(double totaleOrdine) {
+		this.totaleOrdine = totaleOrdine;
+	}
+
+	public String getEmailUtente() {
+		return emailUtente;
+	}
+
+	public void setEmailUtente(String emailUtente) {
+		this.emailUtente = emailUtente;
+	}
+	
 	
 }
